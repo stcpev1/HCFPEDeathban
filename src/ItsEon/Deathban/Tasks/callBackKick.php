558f267b082ad055266ca7137be6c385a9da6f87
ZPlayzMCPE <jameshayward3800@gmail.com>
2018-04-16 13:42:24 +0100

    Update callBackKick.php

--- src/ItsEon/Deathban/Tasks/callBackKick.php
+++ src/ItsEon/Deathban/Tasks/callBackKick.php
@@ -7,7 +7,7 @@ use pocketmine\scheduler\PluginTask;
 class callBackKick extends PluginTask
 {
 	private $main, $player;
-	public function __construct(Main $main, \pocketmine\Player $player)
+	public function __construct(main $main, \pocketmine\Player $player)
 	{
 		parent::__construct($main);
 		$this->main = $main;
