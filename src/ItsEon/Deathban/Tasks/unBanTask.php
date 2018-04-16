b914ad7b3c4d5e37524332cd18d49244e5a1f463
ZPlayzMCPE <jameshayward3800@gmail.com>
2018-04-16 13:42:09 +0100

    Update unBanTask.php

--- src/ItsEon/Deathban/Tasks/unBanTask.php
+++ src/ItsEon/Deathban/Tasks/unBanTask.php
@@ -7,7 +7,7 @@ use pocketmine\scheduler\PluginTask;
 class unBanTask extends PluginTask
 {
 	private $main;
-	public function __construct(Main $main)
+	public function __construct(main $main)
 	{
 		parent::__construct($main);
 		$this->main = $main;
